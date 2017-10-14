1c561d0113ee2ffe08011a9cf4a2d6f099078fd1
Vinoth Pandian <vinothpandian@live.com>
2017-10-14 02:03:08 +0200

    Part 2 Chapter 11 Practice project : 2048

--- P2C113-2048.py
+++ P2C113-2048.py
@@ -1,3 +1,33 @@
 #! python3
 
-import requests
+import sys, bs4
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s : %(levelname)s - %(message)s')
+logging.disable(logging.DEBUG)
+
+browser = webdriver.Firefox()
+URL = 'https://gabrielecirulli.github.io/2048/'
+
+browser.get(URL)
+
+siteBody = browser.find_element_by_tag_name('body')
+
+while True:
+    siteBody.send_keys(Keys.UP)
+    siteBody.send_keys(Keys.RIGHT)
+    siteBody.send_keys(Keys.DOWN)
+    siteBody.send_keys(Keys.LEFT)
+
+    try:
+        browser.find_element_by_class_name('game-over')
+        scoreContainer = browser.find_element_by_class_name('score-container')
+        logging.debug('Container is %s' % scoreContainer.get_attribute("outerHTML"))
+        scoreSoup = bs4.BeautifulSoup(scoreContainer.get_attribute("outerHTML"), "html.parser")
+        logging.debug('Soup is %s' %scoreSoup)
+        score = scoreSoup.find(text=True)
+        print("Your score is %s" % score)
+        break
+    except:
+        continue
